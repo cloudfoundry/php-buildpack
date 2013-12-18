ec300017b498a123344f60de0861465a72f29138
Daniel Mikusa <dmikusa@gopivotal.com>
2013-12-18 07:03:25 -0500

    Added library for httpd specific code and tests.

---
+++ lib/httpd.py
@@ -0,0 +1,48 @@
+import os
+from build_pack_utils import Installer
+from build_pack_utils import CloudFoundryInstaller
+
+
+class HttpdModuleInstaller(object):
+    def __init__(self, builder):
+        self._builder = builder
+        self._installer = CloudFoundryInstaller(self._builder._ctx)
+        self._modules = []
+
+    def _load_modules(self, path):
+        modules = []
+        with open(path, 'rt') as f:
+            for line in f:
+                if line.startswith('LoadModule'):
+                    (cmd, mod, modPath) = line.strip().split(' ')
+                    modules.append(
+                        modPath.split('/')[-1].strip('.so'))
+        return modules
+
+    def from_config(self, path):
+        if os.path.exists(path):
+            if os.path.isdir(path):
+                for root, dirs, files in os.walk(path):
+                    for f in files:
+                        if f.endswith('.conf'):
+                            self._modules.extend(
+                                self._load_modules(
+                                    os.path.join(root, f)))
+            else:
+                assert os.path.isfile(path), "should be a file"
+                self._modules.extend(
+                    self._load_modules(path))
+        else:
+            raise ValueError("path [%s] does not exist" % path)
+
+    def done(self):
+        pass
+
+
+def modules(self):
+    self._module_installer = HttpdModuleInstaller(self.builder)
+
+
+# add methods to Installer
+Installer.modules = modules
+
---
+++ tests/test_httpd.py
@@ -0,0 +1,43 @@
+import os
+from nose.tools import eq_
+from nose.tools import raises
+from nose.tools import with_setup
+from dingus import Dingus
+from httpd import HttpdModuleInstaller
+
+class TestHttpdModuleInstaller(object):
+
+    def setUp(self):
+        self.ctx = {
+            'BUILD_DIR': './test/data',
+            'BP_DIR': './test/data',
+            'CACHE_DIR': '/tmp/cache'
+        }
+        self.builder = Dingus(_ctx=self.ctx)
+
+    @with_setup(setup=setUp)
+    def test_load_modules(self):
+        cfgPath = 'tests/data/httpd/extra/httpd-modules.conf'
+        i = HttpdModuleInstaller(self.builder)
+        eq_(11, len(i._load_modules(cfgPath)))
+
+    @raises(ValueError)
+    @with_setup(setup=setUp)
+    def test_from_config_file_dne(self):
+        cfgPath = 'tests/data/httpd/extra/httpd-dne.conf'
+        i = HttpdModuleInstaller(self.builder)
+        i.from_config(cfgPath)
+
+    @with_setup(setup=setUp)
+    def test_from_config_file(self):
+        cfgPath = 'tests/data/httpd/extra/httpd-modules.conf'
+        i = HttpdModuleInstaller(self.builder)
+        i.from_config(cfgPath)
+        eq_(11, len(i._modules))
+
+    @with_setup(setup=setUp)
+    def test_from_directory(self):
+        cfgPath = 'tests/data/httpd/'
+        i = HttpdModuleInstaller(self.builder)
+        i.from_config(cfgPath)
+        eq_(12, len(i._modules))
