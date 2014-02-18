8b3361e90876c182b1633175d844df4ef070500c
Daniel Mikusa <dmikusa@gopivotal.com>
2014-02-18 16:25:28 -0500

    Initial commit of NewRelic support.

---
+++ extensions/newrelic/extension.py
@@ -0,0 +1,120 @@
+"""NewRelic Extension
+
+Downloads, installs and configures the NewRelic agent for PHP
+"""
+import os
+import os.path
+import logging
+
+
+_log = logging.getLogger('newrelic')
+
+
+DEFAULTS = {
+    'NEWRELIC_HOST': 'download.newrelic.com',
+    'NEWRELIC_VERSION': '4.5.5.38',
+    'NEWRELIC_PACKAGE': 'newrelic-php5-{NEWRELIC_VERSION}-linux.tar.gz',
+    'NEWRELIC_DOWNLOAD_URL': '{DOWNLOAD_URL}/newrelic/{NEWRELIC_PACKAGE}',
+    #'NEWRELIC_DOWNLOAD_URL': 'https://{NEWRELIC_HOST}/php_agent/'
+    #                         'archive/{NEWRELIC_VERSION}/{NEWRELIC_PACKAGE}',
+    'NEWRELIC_HASH_DOWNLOAD_URL': '{DOWNLOAD_URL}/newrelic/'
+                                  '{NEWRELIC_PACKAGE}.{CACHE_HASH_ALGORITHM}',
+    'NEWRELIC_STRIP': True
+}
+
+
+class NewRelicInstaller(object):
+    def __init__(self, ctx):
+        self._log = _log
+        ctx.update(DEFAULTS)
+        self._ctx = ctx
+        if self.should_install():
+            self._log.info("Initializing")
+            self.php_ini_path = os.path.join(self._ctx['BUILD_DIR'],
+                                             'php', 'etc', 'php.ini')
+            self._php_extn_dir = self._find_php_extn_dir()
+            self._php_api, self._php_zts = self._parse_php_api()
+            self._php_arch = ctx.get('NEWRELIC_ARCH', 'x64')
+            self._log.debug("PHP API [%s] Arch [%s]", 
+                            self._php_api, self._php_arch)
+            self.newrelic_so = os.path.join(
+                self._ctx['BUILD_DIR'], 'newrelic',
+                'agent', self._php_arch,
+                'newrelic-%s%s.so' % (self._php_api,
+                                      (self._php_zts and 'zts' or '')))
+            self._log.debug("PHP Extension [%s]", self.newrelic_so)
+            self.app_name = ctx['VCAP_APPLICATION']['name']
+            self._log.debug("App Name [%s]", self.app_name)
+            self.log_path = os.path.join('@{HOME}', '..', 'logs',
+                                         'newrelic-daemon.log')
+            self.daemon_path = os.path.join(
+                '@{HOME}', 'newrelic', 'daemon',
+                'newrelic-daemon.%s' % self._php_arch)
+            self._log.debug("Daemon [%s]", self.daemon_path)
+            self.socket_path = os.path.join('@{HOME}', 'newrelic',
+                                            'daemon.sock')
+            self._log.debug("Socket [%s]", self.socket_path)
+            self.pid_path = os.path.join('@{HOME}', 'newrelic',
+                                         'daemon.pid')
+            self._log.debug("Pid File [%s]", self.pid_path)
+
+    def _find_php_extn_dir(self):
+        with open(self.php_ini_path, 'rt') as php_ini:
+            for line in php_ini.readlines():
+                if line.startswith('extension_dir'):
+                    (key, val) = line.strip().split(' = ')
+                    return val.strip('"')
+
+    def _parse_php_api(self):
+        tmp = os.path.basename(self._php_extn_dir)
+        php_api = tmp.split('-')[-1]
+        php_zts = (tmp.find('non-zts') == -1)
+        return php_api, php_zts
+
+    def should_install(self):
+        return 'NEWRELIC_LICENSE' in self._ctx.keys()
+
+    def modify_php_ini(self):
+        with open(self.php_ini_path, 'rt') as php_ini:
+            lines = php_ini.readlines()
+        extns = [line for line in lines if line.startswith('extension=')]
+        if len(extns) > 0:
+            pos = lines.index(extns[-1]) + 1
+        else:
+            pos = lines.index('#{PHP_EXTENSIONS}\n') + 1
+        lines.insert(pos, 'extension=%s\n' % self.newrelic_so)
+        lines.append('\n')
+        lines.append('[newrelic]\n')
+        lines.append('newrelic.license=%s\n' % self._ctx['NEWRELIC_LICENSE'])
+        lines.append('newrelic.appname=%s\n' % self.app_name)
+        lines.append('newrelic.daemon.logfile=%s\n' % self.log_path)
+        lines.append('newrelic.daemon.location=%s\n' % self.daemon_path)
+        lines.append('newrelic.daemon.port=%s\n' % self.socket_path)
+        lines.append('newrelic.daemon.pidfile=%s\n' % self.pid_path)
+        with open(self.php_ini_path, 'wt') as php_ini:
+            for line in lines:
+                php_ini.write(line)
+
+
+# Extension Methods
+def preprocess_commands(ctx):
+    return ()
+
+
+def service_commands(ctx):
+    return {}
+
+
+def service_environment(ctx):
+    return {}
+
+
+def compile(install):
+    newrelic = NewRelicInstaller(install.builder._ctx)
+    if newrelic.should_install():
+        _log.info("Installing NewRelic")
+        install.package('NEWRELIC')
+        _log.info("Configuring NewRelic in php.ini")
+        newrelic.modify_php_ini()
+        _log.info("NewRelic Installed.")
+    return 0
---
+++ tests/test_newrelic.py
@@ -0,0 +1,262 @@
+import os
+import os.path
+import tempfile
+import shutil
+import json
+from nose.tools import eq_
+from nose.tools import with_setup
+from build_pack_utils import utils
+from build_pack_utils import BuildPack
+
+
+newrelic = utils.load_extension('extensions/newrelic')
+
+
+class TestNewRelic(object):
+    def setUp(self):
+        self.build_dir = tempfile.mkdtemp('build-')
+        self.php_dir = os.path.join(self.build_dir, 'php', 'etc')
+        os.makedirs(self.php_dir)
+        shutil.copy('defaults/config/php/5.4.x/php.ini', self.php_dir)
+
+    def tearDown(self):
+        if os.path.exists(self.build_dir):
+            shutil.rmtree(self.build_dir)
+
+    def testDefaults(self):
+        nr = newrelic.NewRelicInstaller(utils.FormattedDict({}))
+        eq_(True, 'NEWRELIC_HOST' in nr._ctx.keys())
+        eq_(True, 'NEWRELIC_VERSION' in nr._ctx.keys())
+        eq_(True, 'NEWRELIC_PACKAGE' in nr._ctx.keys())
+        eq_(True, 'NEWRELIC_DOWNLOAD_URL' in nr._ctx.keys())
+        eq_(True, 'NEWRELIC_HASH_DOWNLOAD_URL' in nr._ctx.keys())
+        eq_(True, 'NEWRELIC_STRIP' in nr._ctx.keys())
+
+    def testShouldNotInstall(self):
+        nr = newrelic.NewRelicInstaller(utils.FormattedDict({}))
+        eq_(False, nr.should_install())
+
+    @with_setup(setup=setUp, teardown=tearDown)
+    def testShouldInstall(self):
+        ctx = utils.FormattedDict({
+            'BUILD_DIR': self.build_dir,
+            'NEWRELIC_LICENSE': 'JUNK_LICENSE',
+            'VCAP_APPLICATION': {
+                'name': 'app-name-1'
+            }
+        })
+        nr = newrelic.NewRelicInstaller(ctx)
+        eq_(True, nr.should_install())
+        eq_('x64', nr._php_arch)
+        eq_('@{HOME}/php/lib/php/extensions/no-debug-non-zts-20100525',
+            nr._php_extn_dir)
+        eq_(False, nr._php_zts)
+        eq_('20100525', nr._php_api)
+        eq_(os.path.join(self.build_dir, 'newrelic', 'agent', 'x64',
+                         'newrelic-20100525.so'), nr.newrelic_so)
+        eq_('app-name-1', nr.app_name)
+        eq_('@{HOME}/../logs/newrelic-daemon.log', nr.log_path)
+        eq_('@{HOME}/newrelic/daemon/newrelic-daemon.x64', nr.daemon_path)
+        eq_('@{HOME}/newrelic/daemon.sock', nr.socket_path)
+        eq_('@{HOME}/newrelic/daemon.pid', nr.pid_path)
+
+    @with_setup(setup=setUp, teardown=tearDown)
+    def testModifyPhpIni(self):
+        ctx = utils.FormattedDict({
+            'BUILD_DIR': self.build_dir,
+            'NEWRELIC_LICENSE': 'JUNK_LICENSE',
+            'VCAP_APPLICATION': {
+                'name': 'app-name-1'
+            }
+        })
+        nr = newrelic.NewRelicInstaller(ctx)
+        nr.modify_php_ini()
+        with open(os.path.join(self.php_dir, 'php.ini'), 'rt') as php_ini:
+            lines = php_ini.readlines()
+        eq_(True, lines.index('extension=%s\n' % nr.newrelic_so) >= 0)
+        eq_(True, lines.index('[newrelic]\n') >= 0)
+        eq_(True, lines.index('newrelic.license=JUNK_LICENSE\n') >= 0)
+        eq_(True, lines.index('newrelic.appname=%s\n' % nr.app_name) >= 0)
+
+
+class TestCompileNewRelic(object):
+    def _set_web_server(self, optsFile, webServer):
+        options = json.load(open(optsFile))
+        options['WEB_SERVER'] = webServer
+        json.dump(options, open(optsFile, 'wt'))
+
+    def setUp(self):
+        self.build_dir = tempfile.mkdtemp(prefix='build-')
+        self.cache_dir = tempfile.mkdtemp(prefix='cache-')
+        os.rmdir(self.build_dir)  # delete otherwise copytree complains
+        os.rmdir(self.cache_dir)  # cache dir does not exist normally
+        shutil.copytree('tests/data/app-1', self.build_dir)
+
+    def tearDown(self):
+        if os.path.exists(self.build_dir):
+            shutil.rmtree(self.build_dir)
+        if os.path.exists(self.cache_dir):
+            shutil.rmtree(self.cache_dir)
+        for name in os.listdir(os.environ['TMPDIR']):
+            if name.startswith('httpd-') and name.endswith('.gz'):
+                os.remove(os.path.join(os.environ['TMPDIR'], name))
+            if name.startswith('php-') and name.endswith('.gz'):
+                os.remove(os.path.join(os.environ['TMPDIR'], name))
+
+    @with_setup(setup=setUp, teardown=tearDown)
+    def test_setup(self):
+        eq_(True, os.path.exists(self.build_dir))
+        eq_(False, os.path.exists(self.cache_dir))
+        eq_(True, os.path.exists(os.path.join(self.build_dir, 'htdocs')))
+
+    def assert_exists(self, *args):
+        eq_(True, os.path.exists(os.path.join(*args)),
+            "Does not exists: %s" % os.path.join(*args))
+
+    @with_setup(setup=setUp, teardown=tearDown)
+    def test_compile_httpd_with_newrelic(self):
+        os.environ['NEWRELIC_LICENSE'] = 'JUNK_LICENSE'
+        os.environ['VCAP_APPLICATION'] = json.dumps({
+            'name': 'app-name-1'
+        })
+        bp = BuildPack({
+            'BUILD_DIR': self.build_dir,
+            'CACHE_DIR': self.cache_dir,
+        }, '.')
+        # simulate clone, makes debugging easier
+        os.rmdir(bp.bp_dir)
+        shutil.copytree('.', bp.bp_dir,
+                        ignore=shutil.ignore_patterns("binaries",
+                                                      "env",
+                                                      "tests"))
+        # set web server
+        self._set_web_server(os.path.join(bp.bp_dir,
+                                          'defaults',
+                                          'options.json'),
+                             'httpd')
+        try:
+            output = ''
+            output = bp._compile()
+            outputLines = output.split('\n')
+            eq_(21, len(outputLines))
+            for line in outputLines:
+                eq_(True, line.startswith('Download'))
+            # Test scripts and config
+            self.assert_exists(self.build_dir, 'start.sh')
+            with open(os.path.join(self.build_dir, 'start.sh')) as start:
+                lines = [line.strip() for line in start.readlines()]
+                eq_(4, len(lines))
+                eq_('export PYTHONPATH=$HOME/.bp/lib', lines[0])
+                eq_('$HOME/.bp/bin/rewrite "$HOME/httpd/conf"', lines[1])
+                eq_('$HOME/.bp/bin/rewrite "$HOME/php/etc"', lines[2])
+                eq_('$HOME/.bp/bin/start', lines[3])
+            # Check scripts and bp are installed
+            self.assert_exists(self.build_dir, '.bp', 'bin', 'rewrite')
+            self.assert_exists(self.build_dir, '.bp', 'lib')
+            bpu_path = os.path.join(self.build_dir, '.bp', 'lib',
+                                    'build_pack_utils')
+            eq_(22, len(os.listdir(bpu_path)))
+            self.assert_exists(bpu_path, 'utils.py')
+            self.assert_exists(bpu_path, 'process.py')
+            # Check env and procs files
+            self.assert_exists(self.build_dir, '.env')
+            self.assert_exists(self.build_dir, '.procs')
+            with open(os.path.join(self.build_dir, '.env')) as env:
+                lines = [line.strip() for line in env.readlines()]
+                eq_(2, len(lines))
+                eq_('HTTPD_SERVER_ADMIN=dan@mikusa.com', lines[0])
+                eq_('LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/php/lib',
+                    lines[1])
+            with open(os.path.join(self.build_dir, '.procs')) as procs:
+                lines = [line.strip() for line in procs.readlines()]
+                eq_(3, len(lines))
+                eq_('httpd: $HOME/httpd/bin/apachectl -f '
+                    '"$HOME/httpd/conf/httpd.conf" -k start -DFOREGROUND',
+                    lines[0])
+                eq_('php-fpm: $HOME/php/sbin/php-fpm -p "$HOME/php/etc" -y '
+                    '"$HOME/php/etc/php-fpm.conf"', lines[1])
+                eq_('php-fpm-logs: tail -F $HOME/../logs/php-fpm.log',
+                    lines[2])
+            # Check htdocs and config
+            self.assert_exists(self.build_dir, 'htdocs')
+            self.assert_exists(self.build_dir, 'config')
+            self.assert_exists(self.build_dir, 'config', 'options.json')
+            # Test HTTPD
+            self.assert_exists(self.build_dir)
+            self.assert_exists(self.build_dir, 'httpd')
+            self.assert_exists(self.build_dir, 'httpd', 'conf')
+            self.assert_exists(self.build_dir, 'httpd', 'conf', 'httpd.conf')
+            self.assert_exists(self.build_dir, 'httpd', 'conf', 'extra')
+            self.assert_exists(self.build_dir, 'httpd', 'conf',
+                               'extra', 'httpd-modules.conf')
+            self.assert_exists(self.build_dir, 'httpd', 'conf',
+                               'extra', 'httpd-remoteip.conf')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_authz_core.so')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_authz_host.so')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_dir.so')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_env.so')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_log_config.so')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_mime.so')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_mpm_event.so')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_proxy.so')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_proxy_fcgi.so')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_reqtimeout.so')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_unixd.so')
+            self.assert_exists(self.build_dir, 'httpd', 'modules',
+                               'mod_remoteip.so')
+            # Test PHP
+            self.assert_exists(self.build_dir, 'php')
+            self.assert_exists(self.build_dir, 'php', 'etc')
+            self.assert_exists(self.build_dir, 'php', 'etc', 'php-fpm.conf')
+            self.assert_exists(self.build_dir, 'php', 'etc', 'php.ini')
+            self.assert_exists(self.build_dir, 'php', 'sbin', 'php-fpm')
+            self.assert_exists(self.build_dir, 'php', 'bin')
+            self.assert_exists(self.build_dir, 'php', 'bin', 'php-cgi')
+            self.assert_exists(self.build_dir, 'php', 'lib', 'php',
+                               'extensions', 'no-debug-non-zts-20100525',
+                               'bz2.so')
+            self.assert_exists(self.build_dir, 'php', 'lib', 'php',
+                               'extensions', 'no-debug-non-zts-20100525',
+                               'zlib.so')
+            self.assert_exists(self.build_dir, 'php', 'lib', 'php',
+                               'extensions', 'no-debug-non-zts-20100525',
+                               'curl.so')
+            self.assert_exists(self.build_dir, 'php', 'lib', 'php',
+                               'extensions', 'no-debug-non-zts-20100525',
+                               'mcrypt.so')
+            # Test NewRelic
+            self.assert_exists(self.build_dir, 'newrelic')
+            self.assert_exists(self.build_dir, 'newrelic', 'daemon',
+                               'newrelic-daemon.x64')
+            self.assert_exists(self.build_dir, 'newrelic', 'agent', 'x64',
+                               'newrelic-20100525.so')
+            with open(os.path.join(self.build_dir,
+                                   'php', 'etc', 'php.ini'), 'rt') as php_ini:
+                lines = php_ini.readlines()
+            extn_path = os.path.join(self.build_dir, 'newrelic', 'agent',
+                                     'x64', 'newrelic-20100525.so')
+            eq_(True, lines.index('extension=%s\n' % extn_path) >= 0)
+            eq_(True, lines.index('[newrelic]\n') >= 0)
+            eq_(True, lines.index('newrelic.license=JUNK_LICENSE\n') >= 0)
+            eq_(True, lines.index('newrelic.appname=app-name-1\n') >= 0)
+        except Exception, e:
+            print str(e)
+            if hasattr(e, 'output'):
+                print e.output
+            if output:
+                print output
+            raise
+        finally:
+            if os.path.exists(bp.bp_dir):
+                shutil.rmtree(bp.bp_dir)
