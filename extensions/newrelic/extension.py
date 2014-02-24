4885009008f5a6eef4fff0944d70116c1e687915
Daniel Mikusa <dmikusa@gopivotal.com>
2014-02-23 22:05:53 -0500

    Switched in public URLs as defaults.

--- extensions/newrelic/extension.py
+++ extensions/newrelic/extension.py
@@ -14,9 +14,9 @@ DEFAULTS = {
     'NEWRELIC_HOST': 'download.newrelic.com',
     'NEWRELIC_VERSION': '4.5.5.38',
     'NEWRELIC_PACKAGE': 'newrelic-php5-{NEWRELIC_VERSION}-linux.tar.gz',
-    'NEWRELIC_DOWNLOAD_URL': '{DOWNLOAD_URL}/newrelic/{NEWRELIC_PACKAGE}',
-    #'NEWRELIC_DOWNLOAD_URL': 'https://{NEWRELIC_HOST}/php_agent/'
-    #                         'archive/{NEWRELIC_VERSION}/{NEWRELIC_PACKAGE}',
+    #'NEWRELIC_DOWNLOAD_URL': '{DOWNLOAD_URL}/newrelic/{NEWRELIC_PACKAGE}',
+    'NEWRELIC_DOWNLOAD_URL': 'https://{NEWRELIC_HOST}/php_agent/'
+                             'archive/{NEWRELIC_VERSION}/{NEWRELIC_PACKAGE}',
     'NEWRELIC_HASH_DOWNLOAD_URL': '{DOWNLOAD_URL}/newrelic/'
                                   '{NEWRELIC_PACKAGE}.{CACHE_HASH_ALGORITHM}',
     'NEWRELIC_STRIP': True
