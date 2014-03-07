e1f375473e1ff22f8167e55e71946d8260d1e0c3
Daniel Mikusa <dmikusa@gopivotal.com>
2014-03-07 10:25:37 -0500

    Added line to prevent copying manifest.yml to htdocs folder.

--- lib/compile_helpers.py
+++ lib/compile_helpers.py
@@ -31,6 +31,8 @@ def setup_htdocs_if_it_doesnt_exist(ctx):
         fu.where_name_does_not_match(
             '^%s.*$' % os.path.join(ctx['BUILD_DIR'], '.bp-config'))
         fu.where_name_does_not_match(
+            '^%s.*$' % os.path.join(ctx['BUILD_DIR'], 'manifest.yml'))
+        fu.where_name_does_not_match(
             '^%s.*$' % os.path.join(ctx['BUILD_DIR'], 'lib'))
         fu.done()
 
