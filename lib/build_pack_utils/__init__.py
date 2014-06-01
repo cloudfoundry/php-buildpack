cc936e9a0172740ee1543ccb6e0694efb70e3972
Daniel Mikusa <dmikusa@gopivotal.com>
2014-05-31 21:43:24 -0400

    Upgraded build pack utils to #dfc3d88.

--- lib/build_pack_utils/__init__.py
+++ lib/build_pack_utils/__init__.py
@@ -7,4 +7,6 @@ from builder import *
 from zips import *
 from process import Process
 from process import ProcessManager
+from runner import BuildPack
+from runner import check_output
 import utils
