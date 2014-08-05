828dfa29f1389c21b0bccad3d5da5f9bd8b640f7
Daniel Mikusa <dmikusa@gopivotal.com>
2014-08-05 11:26:02 -0400

    Added convenience script to run all tests.

---
+++ run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#  Run all of the tests.
+#
+#  Normally, you could just run `nosetests tests/`, but there are some
+#   odd conflicts when doing this.  The easiest fix is to just run tests
+#   individually.
+#
+set -e
+for TEST in ./tests/*.py; do
+    echo "Running test [$TEST]..."
+    nosetests "$@" "$TEST"
+    echo
+done
