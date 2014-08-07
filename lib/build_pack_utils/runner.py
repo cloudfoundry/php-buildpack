2a9937db792d544dd5ef3842c5305aac5f823337
Daniel Mikusa <dmikusa@gopivotal.com>
2014-08-07 12:42:56 -0400

    Upgraded build pack utils to #7995200.

--- lib/build_pack_utils/runner.py
+++ lib/build_pack_utils/runner.py
@@ -63,6 +63,51 @@ def check_output(*popenargs, **kwargs):
     return output
 
 
+def stream_output(*popenargs, **kwargs):
+    r"""Run command with arguments and stream its output.
+
+    If the exit code was non-zero it raises a CalledProcessError.  The
+    CalledProcessError object will have the return code in the returncode
+    attribute.
+
+    The first argument should be the file like object where the output
+    should be written.  The remainder of the arguments are the same as
+    for the Popen constructor.
+
+    Example:
+
+    >>> fp = open('cmd-output.txt', 'wb')
+    >>> stream_output(fp, ["ls", "-l", "/dev/null"])
+    'crw-rw-rw- 1 root root 1, 3 Oct 18  2007 /dev/null\n'
+
+    The stdout argument is not allowed as it is used internally.
+    To capture standard error in the result, use stderr=STDOUT.
+
+    >>> fp = open('cmd-output.txt', 'wb')
+    >>> stream_output(fp, ["/bin/sh", "-c",
+    ...               "ls -l non_existent_file ; exit 0"],
+    ...               stderr=subprocess.STDOUT)
+    'ls: non_existent_file: No such file or directory\n'
+    """
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    if hasattr(popenargs[0], 'fileno'):
+        process = subprocess.Popen(stdout=popenargs[0],
+                                   *popenargs[1:], **kwargs)
+        retcode = process.wait()
+    else:
+        process = subprocess.Popen(stdout=subprocess.PIPE,
+                                   *popenargs[1:], **kwargs)
+        for c in iter(lambda: process.stdout.read(1024), ''):
+            popenargs[0].write(c)
+        retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise CalledProcessError(retcode, cmd)
+
+
 class BuildPack(object):
     def __init__(self, ctx, url, branch=None):
         self._ctx = ctx
