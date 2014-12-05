5f5ce51b0a27749a31f4df7808cb60194d5d551b
Chris Roemmich <croemmich@myriadmobile.com>
2014-12-05 02:52:36 -0600

    put php in environment path

--- extensions/composer/extension.py
+++ extensions/composer/extension.py
@@ -199,23 +199,26 @@ class ComposerTool(object):
                            delim='@')
         # Run from /tmp/staged/app
         try:
-            phpPath = os.path.join(self._ctx['BUILD_DIR'], 'php', 'bin', 'php')
+            phpDir = os.path.join(self._ctx['BUILD_DIR'], 'php')
+            phpBinDir = os.path.join(phpDir, 'bin')
+            phpPath = os.path.join(phpBinDir, 'php')
             phpCfg = os.path.join(self._ctx['TMPDIR'], 'php.ini')
-            composerPath = os.path.join(self._ctx['BUILD_DIR'], 'php',
-                                        'bin', 'composer.phar')
+            composerPath = os.path.join(phpBinDir, 'composer.phar')
+
             composerEnv = {
-                'LD_LIBRARY_PATH': os.path.join(self._ctx['BUILD_DIR'],
-                                                'php', 'lib'),
+                'PATH': os.getenv('PATH') + os.pathsep + phpBinDir,
+                'LD_LIBRARY_PATH': os.path.join(phpDir, 'lib'),
                 'HOME': self._ctx['BUILD_DIR'],
                 'COMPOSER_VENDOR_DIR': self._ctx['COMPOSER_VENDOR_DIR'],
                 'COMPOSER_BIN_DIR': self._ctx['COMPOSER_BIN_DIR'],
                 'COMPOSER_CACHE_DIR': self._ctx['COMPOSER_CACHE_DIR']
             }
+
             composerCmd = [phpPath,
                            '-c "%s"' % phpCfg,
                            composerPath,
                            'install',
-                           '--no-progress']
+                           '--no-progress', '--no-ansi']
             composerCmd.extend(self._ctx['COMPOSER_INSTALL_OPTIONS'])
             self._log.debug("Running [%s]", ' '.join(composerCmd))
             output = stream_output(sys.stdout,
@@ -224,8 +227,8 @@ class ComposerTool(object):
                                    cwd=self._ctx['BUILD_DIR'],
                                    shell=True)
             _log.debug('composer output [%s]', output)
-        except Exception:
-            _log.error("Command Failed: %s")
+        except Exception as e:
+            _log.error("Composer failed: %s" % e)
 
 
 # Extension Methods
