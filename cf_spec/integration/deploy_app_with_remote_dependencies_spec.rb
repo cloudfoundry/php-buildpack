be0e83a98ca67852fddda0f01105c0a9b89450d8
JT Archie and Rasheed Abdul-Aziz <pair+jarchie+squeedee@pivotal.io>
2015-01-06 11:26:07 -0500

    Add composer remote dependency test for composer
    [#85446076]

---
+++ cf_spec/integration/deploy_app_with_remote_dependencies_spec.rb
@@ -0,0 +1,23 @@
+$: << 'cf_spec'
+require 'cf_spec_helper'
+
+describe 'CF PHP Buildpack' do
+  subject(:app) { Machete.deploy_app(app_name) }
+  let(:browser) { Machete::Browser.new(app) }
+
+  after do
+    Machete::CF::DeleteApp.new.execute(app)
+  end
+
+  context 'the application has remote dependencies', if: Machete::BuildpackMode.online? do
+    let(:app_name) { 'app_with_remote_dependencies' }
+
+    specify do
+      expect(app).to be_running
+
+      browser.visit_path("/")
+      expect(browser).to have_body 'App with remote dependencies running'
+    end
+  end
+end
+
