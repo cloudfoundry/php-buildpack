fa0abb7c953612f36797f361389cb2fd813443b1
Jacques Chester and Sai To Yeung <pair+jchester+syeung@pivotal.io>
2015-01-15 10:29:26 -0500

    Migrate Zend integration test from legacy buildpack
    - Add local dependency and remote dependency test fixtures from legacy buildpack. Removed Procfiles.
    - Add .bp-config/options.json to correct configure paths.
    
    [#84393666]

---
+++ cf_spec/fixtures/zend_framework_hello_world_with_remote_dependencies/config/autoload/navigation.global.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+    'navigation' => array(
+        'default' => array(
+            array(
+                'label' => 'Home',
+                'route' => 'home',
+            ),
+            array(
+                'label' => 'Login',
+                'route' => 'scn-social-auth-user/login',
+            ),
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory',
+        ),
+    ),
+);
---
+++ cf_spec/integration/deploy_zend_app_spec.rb
@@ -0,0 +1,35 @@
+$: << 'cf_spec'
+require 'cf_spec_helper'
+
+describe 'CF PHP Buildpack' do
+  subject(:app) { Machete.deploy_app(app_name) }
+  let(:browser) { Machete::Browser.new(app) }
+
+  after do
+    Machete::CF::DeleteApp.new.execute(app)
+  end
+
+  context 'deploying a Zend app with locally-vendored dependencies', if: Machete::BuildpackMode.offline? do
+    let(:app_name) { 'zend_framework_hello_world_with_local_dependencies' }
+
+    specify do
+      expect(app).to be_running
+
+      browser.visit_path("/")
+      expect(browser).to have_body 'Zend Framework 2'
+
+      expect(app.host).not_to have_internet_traffic
+    end
+  end
+
+  context 'deploying a Zend app with remote dependencies', if: Machete::BuildpackMode.online? do
+    let(:app_name) { 'zend_framework_hello_world_with_remote_dependencies' }
+
+    specify do
+      expect(app).to be_running
+
+      browser.visit_path("/")
+      expect(browser).to have_body 'Zend Framework 2'
+    end
+  end
+end
