f9e80c55e901e8df471f9a4c25f9849e7741da7d
Daniel Mikusa and Jacques Chester <pair+dmikusa+jchester@pivotal.io>
2015-01-26 16:31:11 -0500

    WIP: allow composer env vars to be config'd by user with set-env

--- tests/test_composer.py
+++ tests/test_composer.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 from nose.tools import eq_
 from dingus import Dingus
@@ -470,3 +471,34 @@ class TestComposer(object):
         ct = self.ct.ComposerExtension(ctx)
         path = ct.binary_path()
         eq_('/usr/awesome/php/bin/php', path)
+
+    def test_build_composer_environment_inherits_from_ctx(self):
+        ctx = utils.FormattedDict({
+            'BUILD_DIR': '/usr/awesome',
+            'PHPRC': '/usr/awesome/phpini',
+            'PHP_VM': 'php',
+            'TMPDIR': 'tmp',
+            'OUR_SPECIAL_KEY': 'SPECIAL_VALUE'
+        })
+        oldenv = os.environ
+        try:
+            os.environ = {'OUR_SPECIAL_KEY': 'ORIGINAL_SPECIAL_VALUE'}
+            ct = self.ct.ComposerExtension(ctx)
+            built_environment = ct._build_composer_environment()
+        finally:
+            os.environ = oldenv
+        assert 'OUR_SPECIAL_KEY' in built_environment, \
+            'OUR_SPECIAL_KEY was not found in the built_environment variable'
+
+    def test_build_composer_environment_forbids_overwriting_key_vars(self):
+        ctx = utils.FormattedDict({
+            'BUILD_DIR': '/usr/awesome',
+            'PHP_VM': 'php',
+            'TMPDIR': 'tmp',
+            'PHPRC': '/usr/awesome/phpini',
+        })
+        ct = self.ct.ComposerExtension(ctx)
+        built_environment = ct._build_composer_environment()
+
+        eq_(built_environment['LD_LIBRARY_PATH'], '/usr/awesome/php/lib')
+        eq_(built_environment['PHPRC'], 'tmp')
