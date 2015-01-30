ca65d30dfabc5c7472003f4769049e2587e90002
Jacques Chester and Rasheed Abdul-Aziz <pair+jchester+squeedee@pivotal.io>
2015-01-30 16:11:05 -0500

    Update unit tests to work with reraised Composer exceptions
    - Remove test_composer_tool_run_with_{php|hhvm}() and test_composer_tool_run_with_hhvm(). These were integration tests
    mostly duplicating functionality now found in cf_spec/.
    - Added an assertion to test_composer_run_streams_output() to capture an assertion that was in test_composer_tool_run_with_{php|hhvm}.
    - Rename some variables
    - Use more idiomatic Python re-raise
    
    [#85465540]

--- tests/test_composer.py
+++ tests/test_composer.py
@@ -5,11 +5,10 @@ from dingus import Dingus
 from dingus import patch
 from build_pack_utils import utils
 
-
 class TestComposer(object):
 
     def __init__(self):
-        self.ct = utils.load_extension('extensions/composer')
+        self.extension_module = utils.load_extension('extensions/composer')
 
     def test_composer_tool_should_compile(self):
         ctx = utils.FormattedDict({
@@ -20,7 +19,7 @@ class TestComposer(object):
             'WEBDIR': 'htdocs',
             'LIBDIR': 'lib'
         })
-        ct = self.ct.ComposerExtension(ctx)
+        ct = self.extension_module.ComposerExtension(ctx)
         assert ct._should_compile()
 
     def test_composer_tool_should_compile_not_found(self):
@@ -32,7 +31,7 @@ class TestComposer(object):
             'WEBDIR': 'htdocs',
             'LIBDIR': 'lib'
         })
-        ct = self.ct.ComposerExtension(ctx)
+        ct = self.extension_module.ComposerExtension(ctx)
         assert not ct._should_compile()
 
     def test_composer_tool_install(self):
@@ -48,7 +47,7 @@ class TestComposer(object):
         cfInstaller = Dingus()
         builder.install = Dingus(_installer=cfInstaller,
                                  return_value=installer)
-        ct = self.ct.ComposerExtension(ctx)
+        ct = self.extension_module.ComposerExtension(ctx)
         ct._builder = builder
         ct.install()
         eq_(2, len(builder.install.calls()))
@@ -58,62 +57,6 @@ class TestComposer(object):
         assert call.return_value.calls().once()
         eq_('cli', call.return_value.calls()[0].args[0])
         assert installer.calls().once()
-
-    def test_composer_tool_run_with_php(self):
-        ctx = utils.FormattedDict({
-            'PHP_VM': 'php',
-            'DOWNLOAD_URL': 'http://server/bins',
-            'CACHE_HASH_ALGORITHM': 'sha1',
-            'BUILD_DIR': '/build/dir',
-            'CACHE_DIR': '/cache/dir',
-            'TMPDIR': tempfile.gettempdir(),
-            'WEBDIR': 'htdocs',
-            'LIBDIR': 'lib'
-        })
-        builder = Dingus(_ctx=ctx)
-        # patch utils.rewrite_cfg method
-        old_rewrite = self.ct.utils.rewrite_cfgs
-        rewrite = Dingus()
-        self.ct.utils.rewrite_cfgs = rewrite
-        try:
-            ct = self.ct.ComposerExtension(ctx)
-            ct._builder = builder
-            ct.run()
-            eq_(2, len(builder.move.calls()))
-            eq_(1, len(builder.copy.calls()))
-            assert rewrite.calls().once()
-            rewrite_args = rewrite.calls()[0].args
-            assert rewrite_args[0].endswith('php.ini')
-            assert 'HOME' in rewrite_args[1]
-            assert 'TMPDIR' in rewrite_args[1]
-        finally:
-            self.ct.utils.rewrite_cfgs = old_rewrite
-
-    def test_composer_tool_run_with_hhvm(self):
-        ctx = utils.FormattedDict({
-            'PHP_VM': 'hhvm',
-            'DOWNLOAD_URL': 'http://server/bins',
-            'CACHE_HASH_ALGORITHM': 'sha1',
-            'BUILD_DIR': '/build/dir',
-            'CACHE_DIR': '/cache/dir',
-            'TMPDIR': tempfile.gettempdir(),
-            'WEBDIR': 'htdocs',
-            'LIBDIR': 'lib'
-        })
-        builder = Dingus(_ctx=ctx)
-        # patch utils.rewrite_cfg method
-        old_rewrite = self.ct.utils.rewrite_cfgs
-        rewrite = Dingus()
-        self.ct.utils.rewrite_cfgs = rewrite
-        try:
-            ct = self.ct.ComposerExtension(ctx)
-            ct._builder = builder
-            ct.run()
-            eq_(2, len(builder.move.calls()))
-            eq_(0, len(builder.copy.calls()))
-            eq_(0, len(rewrite.calls()))
-        finally:
-            self.ct.utils.rewrite_cfgs = old_rewrite
 
     def test_composer_run_streams_output(self):
         ctx = utils.FormattedDict({
@@ -128,21 +71,22 @@ class TestComposer(object):
         })
         builder = Dingus(_ctx=ctx)
         # patch stream_output method
-        old_stream_output = self.ct.stream_output
+        old_stream_output = self.extension_module.stream_output
         co = Dingus()
-        self.ct.stream_output = co
+        self.extension_module.stream_output = co
         try:
-            ct = self.ct.ComposerExtension(ctx)
+            ct = self.extension_module.ComposerExtension(ctx)
             ct._builder = builder
             ct.run()
             assert co.calls().once()
             instCmd = co.calls()[0].args[1]
+            assert instCmd.find('/build/dir/php/bin/composer.phar') > 0
             assert instCmd.find('install') > 0
             assert instCmd.find('--no-progress') > 0
             assert instCmd.find('--no-interaction') > 0
             assert instCmd.find('--no-dev') > 0
         finally:
-            self.ct.stream_output = old_stream_output
+            self.extension_module.stream_output = old_stream_output
 
     def test_composer_tool_run_custom_composer_opts(self):
         ctx = utils.FormattedDict({
@@ -158,15 +102,15 @@ class TestComposer(object):
         })
         builder = Dingus(_ctx=ctx)
         # patch stream_output method
-        old_stream_output = self.ct.stream_output
+        old_stream_output = self.extension_module.stream_output
         co = Dingus()
-        self.ct.stream_output = co
+        self.extension_module.stream_output = co
         # patch utils.rewrite_cfg method
-        old_rewrite = self.ct.utils.rewrite_cfgs
+        old_rewrite = self.extension_module.utils.rewrite_cfgs
         rewrite = Dingus()
-        self.ct.utils.rewrite_cfgs = rewrite
+        self.extension_module.utils.rewrite_cfgs = rewrite
         try:
-            ct = self.ct.ComposerExtension(ctx)
+            ct = self.extension_module.ComposerExtension(ctx)
             ct._builder = builder
             ct.run()
             eq_(2, len(builder.move.calls()))
@@ -184,58 +128,63 @@ class TestComposer(object):
             assert instCmd.find('--no-dev') == -1
             assert instCmd.find('--optimize-autoloader') > 0
         finally:
-            self.ct.stream_output = old_stream_output
-            self.ct.utils.rewrite_cfgs = old_rewrite
+            self.extension_module.stream_output = old_stream_output
+            self.extension_module.utils.rewrite_cfgs = old_rewrite
 
     def test_composer_tool_run_sanity_checks(self):
-        ctx = utils.FormattedDict({
+        context = utils.FormattedDict({
             'PHP_VM': 'php',
             'DOWNLOAD_URL': 'http://server/bins',
             'CACHE_HASH_ALGORITHM': 'sha1',
             'BUILD_DIR': '/build/dir',
             'CACHE_DIR': '/cache/dir',
-            'TMPDIR': tempfile.gettempdir()
+            'TMPDIR': tempfile.gettempdir(),
+            'LIBDIR': 'lib'
         })
-        builder = Dingus(_ctx=ctx)
+        builder = Dingus(_ctx=context)
+
         # patch stream_output method
-        old_stream_output = self.ct.stream_output
-        co = Dingus()
-        self.ct.stream_output = co
+        old_stream_output = self.extension_module.stream_output
+        stream_output_stub = Dingus()
+        self.extension_module.stream_output = stream_output_stub
+
         # patch utils.rewrite_cfg method
-        old_rewrite = self.ct.utils.rewrite_cfgs
-        rewrite = Dingus()
-        self.ct.utils.rewrite_cfgs = rewrite
+        old_rewrite = self.extension_module.utils.rewrite_cfgs
+        rewrite_stub = Dingus()
+        self.extension_module.utils.rewrite_cfgs = rewrite_stub
+
         try:
-            ct = self.ct.ComposerExtension(ctx)
-            ct._log = Dingus()
-            ct._builder = builder
-            ct.run()
-            assert len(ct._log.warning.calls()) > 0
-            assert ct._log.warning.calls()[0].args[0].find('PROTIP:') == 0
+            composer_extension = self.extension_module.ComposerExtension(context)
+            composer_extension._log = Dingus()
+            composer_extension._builder = builder
+            composer_extension.run()
+
+            assert len(composer_extension._log.warning.calls()) > 0
+            assert composer_extension._log.warning.calls()[0].args[0].find('PROTIP:') == 0
             exists = Dingus(return_value=True)
             with patch('os.path.exists', exists):
-                ct._log = Dingus()
-                ct.run()
+                composer_extension._log = Dingus()
+                composer_extension.run()
             assert len(exists.calls()) == 1
-            assert len(ct._log.warning.calls()) == 0
+            assert len(composer_extension._log.warning.calls()) == 0
         finally:
-            self.ct.stream_output = old_stream_output
-            self.ct.utils.rewrite_cfgs = old_rewrite
+            self.extension_module.stream_output = old_stream_output
+            self.extension_module.utils.rewrite_cfgs = old_rewrite
 
     def test_process_commands(self):
-        eq_(0, len(self.ct.preprocess_commands({
+        eq_(0, len(self.extension_module.preprocess_commands({
             'BUILD_DIR': '',
             'PHP_VM': ''
             })))
 
     def test_service_commands(self):
-        eq_(0, len(self.ct.service_commands({
+        eq_(0, len(self.extension_module.service_commands({
             'BUILD_DIR': '',
             'PHP_VM': ''
             })))
 
     def test_service_environment(self):
-        eq_(0, len(self.ct.service_environment({
+        eq_(0, len(self.extension_module.service_environment({
             'BUILD_DIR': '',
             'PHP_VM': ''
             })))
@@ -245,7 +194,7 @@ class TestComposer(object):
             'BUILD_DIR': 'tests/data/composer',
             'PHP_54_LATEST': '5.4.31'
         })
-        config = self.ct.ComposerConfiguration(ctx)
+        config = self.extension_module.ComposerConfiguration(ctx)
         config.configure()
         assert 'PHP_EXTENSIONS' in ctx.keys()
         assert list == type(ctx['PHP_EXTENSIONS'])
@@ -263,7 +212,7 @@ class TestComposer(object):
             'PHP_EXTENSIONS': ['a', 'b'],
             'PHP_54_LATEST': '5.4.31'
         })
-        config = self.ct.ComposerConfiguration(ctx)
+        config = self.extension_module.ComposerConfiguration(ctx)
         config.configure()
         assert 'PHP_EXTENSIONS' in ctx.keys()
         assert list == type(ctx['PHP_EXTENSIONS'])
@@ -282,7 +231,7 @@ class TestComposer(object):
             'BUILD_DIR': 'tests/data/composer-no-php',
             'PHP_VERSION': '5.4.31'  # uses bp default
         })
-        config = self.ct.ComposerConfiguration(ctx)
+        config = self.extension_module.ComposerConfiguration(ctx)
         config.configure()
         assert '5.4.31' == ctx['PHP_VERSION']
         assert 'php' == ctx['PHP_VM']
@@ -298,7 +247,7 @@ class TestComposer(object):
             'BUILD_DIR': 'tests/data/composer-with-hhvm',
             'HHVM_VERSION': '3.2.0'
         })
-        config = self.ct.ComposerConfiguration(ctx)
+        config = self.extension_module.ComposerConfiguration(ctx)
         config.configure()
         assert '3.2.0' == ctx['HHVM_VERSION']
         assert 'hhvm' == ctx['PHP_VM']
@@ -308,7 +257,7 @@ class TestComposer(object):
             'BUILD_DIR': 'tests/data/app-1',
             'PHP_EXTENSIONS': ['a', 'b']
         })
-        config = self.ct.ComposerConfiguration(ctx)
+        config = self.extension_module.ComposerConfiguration(ctx)
         config.configure()
         assert 'PHP_EXTENSIONS' in ctx.keys()
         assert list == type(ctx['PHP_EXTENSIONS'])
@@ -332,29 +281,29 @@ class TestComposer(object):
             'BUILD_DIR': 'tests/data/composer',
             'PHP_54_LATEST': '5.4.31'
         })
-        fcp_orig = self.ct.find_composer_paths
+        fcp_orig = self.extension_module.find_composer_paths
         # test when no composer.json or composer.lock files found
-        self.ct.find_composer_paths = fcp_test_none
+        self.extension_module.find_composer_paths = fcp_test_none
         try:
-            self.ct.ComposerConfiguration(ctx).configure()
+            self.extension_module.ComposerConfiguration(ctx).configure()
             assert 'PHP_EXTENSIONS' not in ctx.keys()
         finally:
-            self.ct.find_composer_paths = fcp_orig
+            self.extension_module.find_composer_paths = fcp_orig
         # test when composer.json found, but no composer.lock
-        self.ct.find_composer_paths = fcp_test_json
+        self.extension_module.find_composer_paths = fcp_test_json
         try:
-            self.ct.ComposerConfiguration(ctx).configure()
+            self.extension_module.ComposerConfiguration(ctx).configure()
             assert 'PHP_EXTENSIONS' in ctx.keys()
             assert 3 == len(ctx['PHP_EXTENSIONS'])
             assert 'openssl' in ctx['PHP_EXTENSIONS']
             assert 'fileinfo' in ctx['PHP_EXTENSIONS']
             assert 'zip' in ctx['PHP_EXTENSIONS']
         finally:
-            self.ct.find_composer_paths = fcp_orig
+            self.extension_module.find_composer_paths = fcp_orig
         # test when composer.lock found, but no composer.json
-        self.ct.find_composer_paths = fcp_test_lock
+        self.extension_module.find_composer_paths = fcp_test_lock
         try:
-            self.ct.ComposerConfiguration(ctx).configure()
+            self.extension_module.ComposerConfiguration(ctx).configure()
             assert 'PHP_EXTENSIONS' in ctx.keys()
             assert 4 == len(ctx['PHP_EXTENSIONS'])
             assert 'openssl' in ctx['PHP_EXTENSIONS']
@@ -362,11 +311,11 @@ class TestComposer(object):
             assert 'fileinfo' in ctx['PHP_EXTENSIONS']
             assert 'zip' in ctx['PHP_EXTENSIONS']
         finally:
-            self.ct.find_composer_paths = fcp_orig
+            self.extension_module.find_composer_paths = fcp_orig
 
     def test_find_composer_paths(self):
         (json_path, lock_path) = \
-            self.ct.find_composer_paths('tests')
+            self.extension_module.find_composer_paths('tests')
         assert json_path is not None
         assert lock_path is not None
         eq_('tests/data/composer/composer.json', json_path)
@@ -374,7 +323,7 @@ class TestComposer(object):
 
     def test_find_composer_php_version(self):
         ctx = {'BUILD_DIR': 'tests'}
-        config = self.ct.ComposerConfiguration(ctx)
+        config = self.extension_module.ComposerConfiguration(ctx)
         php_version = config.read_version_from_composer_json('php')
         eq_('>=5.3', php_version)
         # check lock file
@@ -388,7 +337,7 @@ class TestComposer(object):
             'BUILD_DIR': '',
             'PHP_55_LATEST': '5.5.15'
         }
-        pick_php_version = self.ct.ComposerConfiguration(ctx).pick_php_version
+        pick_php_version = self.extension_module.ComposerConfiguration(ctx).pick_php_version
         # no PHP 5.3, default to 5.4
         eq_('5.4.31', pick_php_version('>=5.3'))
         eq_('5.4.31', pick_php_version('5.3.*'))
@@ -413,7 +362,7 @@ class TestComposer(object):
         eq_('5.4.31', pick_php_version('<5.4'))
 
     def test_empty_platform_section(self):
-        exts = self.ct.ComposerConfiguration({
+        exts = self.extension_module.ComposerConfiguration({
             'BUILD_DIR': ''
         }).read_exts_from_path(
             'tests/data/composer/composer-phalcon.lock')
@@ -422,7 +371,7 @@ class TestComposer(object):
         eq_('tokenizer', exts[1])
 
     def test_none_for_extension_reading(self):
-        exts = self.ct.ComposerConfiguration({
+        exts = self.extension_module.ComposerConfiguration({
             'BUILD_DIR': ''
         }).read_exts_from_path(None)
         eq_(0, len(exts))
@@ -434,7 +383,7 @@ class TestComposer(object):
             'PHP_VM': 'will_default_to_php_strategy',
             'LIBDIR': 'lib'
         })
-        ct = self.ct.ComposerExtension(ctx)
+        ct = self.extension_module.ComposerExtension(ctx)
         eq_('/tmp/build/lib/vendor', ct._ctx['COMPOSER_VENDOR_DIR'])
         eq_('/tmp/build/php/bin', ct._ctx['COMPOSER_BIN_DIR'])
         eq_('/tmp/cache/composer', ct._ctx['COMPOSER_CACHE_DIR'])
@@ -449,7 +398,7 @@ class TestComposer(object):
             'PHP_VM': 'will_default_to_php_strategy',
             'COMPOSER_CACHE_DIR': '{CACHE_DIR}/custom'
         })
-        ct = self.ct.ComposerExtension(ctx)
+        ct = self.extension_module.ComposerExtension(ctx)
         eq_('/tmp/build/vendor', ct._ctx['COMPOSER_VENDOR_DIR'])
         eq_('/tmp/build/bin', ct._ctx['COMPOSER_BIN_DIR'])
         eq_('/tmp/cache/custom', ct._ctx['COMPOSER_CACHE_DIR'])
@@ -459,7 +408,7 @@ class TestComposer(object):
             'BUILD_DIR': '/usr/awesome/',
             'PHP_VM': 'hhvm'
         })
-        ct = self.ct.ComposerExtension(ctx)
+        ct = self.extension_module.ComposerExtension(ctx)
         path = ct.binary_path()
         eq_('/usr/awesome/hhvm/usr/bin/hhvm', path)
 
@@ -468,7 +417,7 @@ class TestComposer(object):
             'BUILD_DIR': '/usr/awesome',
             'PHP_VM': 'php'
         })
-        ct = self.ct.ComposerExtension(ctx)
+        ct = self.extension_module.ComposerExtension(ctx)
         path = ct.binary_path()
         eq_('/usr/awesome/php/bin/php', path)
 
