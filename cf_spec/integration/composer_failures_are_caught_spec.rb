3c499bfc46f65239cf0fda39a97ec9ca7ea34617
Jacques Chester and Rasheed Abdul-Aziz <pair+jchester+squeedee@pivotal.io>
2015-01-30 11:56:15 -0500

    Staging fails on composer errors.
    [#85465540]

---
+++ cf_spec/integration/composer_failures_are_caught_spec.rb
@@ -0,0 +1,22 @@
+$: << 'cf_spec'
+require 'cf_spec_helper'
+
+describe 'Composer failures' do
+  subject(:app) { Machete.deploy_app(app_name) }
+  let(:browser) { Machete::Browser.new(app) }
+
+  # after do
+  #   Machete::CF::DeleteApp.new.execute(app)
+  # end
+
+  context 'deploying an app with an impossible dependency in composer.json', if: Machete::BuildpackMode.online? do
+    let(:app_name) { 'composer_with_impossible_dependency' }
+
+    specify do
+      expect(app).to_not be_running
+
+      expect(app).to have_logged '-----> Composer command failed'
+    end
+  end
+end
+
