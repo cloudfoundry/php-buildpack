e632ce5492ebbc2c9a2493872b6a4ad5eeac8312
JT Archie <jtarchie@gmail.com>
2015-03-18 15:16:45 -0400

    fix test referencing incorrect names for stacks

--- cf_spec/integration/deploy_a_php_app_with_different_stacks_spec.rb
+++ cf_spec/integration/deploy_a_php_app_with_different_stacks_spec.rb
@@ -9,22 +9,24 @@ describe 'CF PHP Buildpack with with different stacks' do
     Machete::CF::DeleteApp.new.execute(app)
   end
 
-  context 'deploying a basic PHP app on a lucid64 rootfs', if: ENV['CF_STACK'] == 'lucid' do
+  context 'deploying a basic PHP app on a lucid64 rootfs', if: ENV['CF_STACK'] == 'lucid64' do
     let(:app_name) { 'php_app' }
 
     specify do
       expect(app).to be_running
 
       expect(app).to have_logged 'lucid'
+      expect(app).to_not have_logged 'trusty'
     end
   end
 
-  context 'deploying a basic PHP app on a cflinuxfs2 rootfs', if: ENV['CF_STACK'] == 'trusty' do
+  context 'deploying a basic PHP app on a cflinuxfs2 rootfs', if: ENV['CF_STACK'] == 'cflinuxfs2' do
     let(:app_name) { 'php_app' }
 
     specify do
       expect(app).to be_running
 
+      expect(app).to_not have_logged 'lucid'
       expect(app).to have_logged 'trusty'
     end
   end
