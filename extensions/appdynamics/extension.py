9acd2c4e58b82beed4d0e6e1f978d5cf4403cbf4
Sri Dharmasanam <sri.dharmasanam@appdynamics.com>
2015-10-14 09:29:30 -0400

    avoid use of ADDITIONAL_PREPROCESS_CMDS since its not supported for extensions

--- extensions/appdynamics/extension.py
+++ extensions/appdynamics/extension.py
@@ -174,11 +174,10 @@ def preprocess_commands(ctx):
         #    'ADDITIONAL_PREPROCESS_CMDS':  ['env', 'ls -al']
         #}
 
-    return ((
-	'echo',
-	'" in preprocess;"',
-	'env', 
-	'ls'),)
+    return [
+	[ 'echo', '" in preprocess;"'],
+	['env'], 
+	['ls']]
 
     #return ()
 
