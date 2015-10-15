83651bbb63a46d12dc90b68c8538ce4ca6d513e1
Sri Dharmasanam <sri.dharmasanam@appdynamics.com>
2015-10-15 06:10:48 -0400

    avoid use of ADDITIONAL_PREPROCESS_CMDS since its not supported for extensions

--- extensions/appdynamics/extension.py
+++ extensions/appdynamics/extension.py
@@ -173,7 +173,7 @@ def preprocess_commands(ctx):
     return [
 	[ 'echo', '" in preprocess;"'],
 	['env'], 
-	[ 'chmod', '" -R 755 /home/vcap/app"'],
+	[ 'chmod', ' -R 755 /home/vcap/app'],
 	[ 'chmod', '" 777 ./app/appdynamics/appdynamics-php-agent/logs"'],
 	[ 'export', '"APP_TIERNAME=`echo $VCAP_APPLICATION | sed -e \'s/.*application_name.:.//g;s/\".*application_uri.*//g\' `"'],
 	['"if [ -z $application_name ]; then export APP_NAME=$APP_TIERNAME && APP_TIERNAME=$APP_TIERNAME-tier; else export APP_NAME=$application_name; fi"'],
