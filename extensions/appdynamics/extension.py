77dab059263d7df53b217a4ad76f922e73fe737b
Sri Dharmasanam <sri.dharmasanam@appdynamics.com>
2015-10-19 08:15:47 -0400

    modified uri for agent download

--- extensions/appdynamics/extension.py
+++ extensions/appdynamics/extension.py
@@ -172,8 +172,7 @@ def preprocess_commands(ctx):
 
     if detected == True: 
     	exit_code = os.system("echo !!! preprocess_commands: AppDynamics agent configuration")
-    	return [ ['env'], 
-		['env']]
+    	return [[ 'echo', '" in preprocess;"']]
 
 def service_commands(ctx):
     return {}
