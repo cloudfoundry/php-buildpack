5587f0921cb006ac17e9743ccf9ca030f3b9ad7d
David Jahn <david.a.jahn@gmail.com>
2016-04-26 16:51:29 -0400

    Add test specs for symfony 2.8 [#116972231]
    Signed-off-by: Gabriel Ramirez <gabriel.e.ramirez@gmail.com>

--- cf_spec/integration/deploy_symfony_2_app_spec.rb
+++ cf_spec/integration/deploy_symfony_2_app_spec.rb
@@ -4,7 +4,7 @@ require 'cf_spec_helper'
 describe 'CF PHP Buildpack' do
   let(:browser) { Machete::Browser.new(@app) }
 
-  context 'deploying a symfony 2 app with locally-vendored dependencies', :cached do
+  context 'deploying a symfony 2.1 app with locally-vendored dependencies', :cached do
     before(:context) do
       @app = Machete.deploy_app('symfony_2_hello_world_with_local_dependencies', {})
     end
@@ -32,7 +32,7 @@ describe 'CF PHP Buildpack' do
     # end
   end
 
-  context 'deploying a symfony 2 app with remotely-sourced dependencies', :uncached do
+  context 'deploying a symfony 2.1 app with remotely-sourced dependencies', :uncached do
     before(:context) do
       @app = Machete.deploy_app('symfony_2_hello_world_with_remote_dependencies', {
         env: {
@@ -50,6 +50,52 @@ describe 'CF PHP Buildpack' do
     end
 
     it 'dynamically generates the content for the root route' do
+      browser.visit_path("/")
+      expect(browser).to have_body 'Running on Symfony!'
+    end
+  end
+
+  context 'deploying a symfony 2.8 app with locally-vendored dependencies', :cached do
+    before(:context) do
+      @app = Machete.deploy_app('symfony_2.8_with_remote_dependencies', {})
+    end
+
+    after(:context) do
+      Machete::CF::DeleteApp.new.execute(@app)
+    end
+
+    it 'expects an app to be running' do
+      expect(@app).to be_running
+    end
+
+    it 'that root route has content that is dynamically generated' do
+      browser.visit_path("/")
+      expect(browser).to have_body 'Running on Symfony!'
+    end
+
+    # it 'does not call out to the internet during staging' do
+    #   expect(@app).not_to have_internet_traffic
+    # end
+  end
+
+  context 'deploying a symfony 2.8 app with remotely-sourced dependencies', :uncached do
+    before(:context) do
+      @app = Machete.deploy_app('symfony_2.8_with_remote_dependencies', {
+        env: {
+          'COMPOSER_GITHUB_OAUTH_TOKEN' => ENV['COMPOSER_GITHUB_OAUTH_TOKEN']
+        }
+      })
+    end
+
+    after(:context) do
+      Machete::CF::DeleteApp.new.execute(@app)
+    end
+
+    it 'expects an app to be running' do
+      expect(@app).to be_running
+    end
+
+    it 'that root route has content that is dynamically generated' do
       browser.visit_path("/")
       expect(browser).to have_body 'Running on Symfony!'
     end
