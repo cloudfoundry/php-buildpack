b853417e6889ff1fe95dfe6133289c8c6a811da7
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-08-11 15:27:17 -0400

    Test that all php modules can be loaded
    - For php5 and for php7
    - when specified in options.json or when specified in composer.json
    
    [#127809787]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- cf_spec/integration/deploy_a_php_app_with_all_modules.rb
+++ cf_spec/integration/deploy_a_php_app_with_all_modules.rb
@@ -4,44 +4,79 @@ require 'yaml'
 
 php_buildpack_manifest_file = File.join(File.expand_path(File.dirname(__FILE__)), '..', '..', 'manifest.yml')
 manifest = YAML.load(File.read(php_buildpack_manifest_file))
-php_version_5 = manifest['dependencies'].select { |dependency| dependency['name'] == "php" && dependency['version'].to_s[0] == '5' }.first
-php_version_7 = manifest['dependencies'].select { |dependency| dependency['name'] == "php" && dependency['version'].to_s[0] == '7' }.first
+
+php_modules = {}
+php_modules['PHP 5'] = manifest['dependencies'].select { |dependency| dependency['name'] == "php" && dependency['version'].to_s[0] == '5' }.first
+php_modules['PHP 7'] = php_version_7 = manifest['dependencies'].select { |dependency| dependency['name'] == "php" && dependency['version'].to_s[0] == '7' }.first
+
+def setup_app(app_name)
+    env_config = {env:  {'COMPOSER_GITHUB_OAUTH_TOKEN' => ENV['COMPOSER_GITHUB_OAUTH_TOKEN']}}
+    deploy_app(app_name, env_config)
+end
+
 
 describe 'CF PHP Buildpack' do
-  let(:browser) { Machete::Browser.new(@app) }
+  let(:browser)    { Machete::Browser.new(@app) }
 
-  before do
-    @app = Machete.deploy_app(
-      app_name,
-      {env: {'COMPOSER_GITHUB_OAUTH_TOKEN' => ENV['COMPOSER_GITHUB_OAUTH_TOKEN']}}
-    )
-  end
-
-  after(:context) do
-   # Machete::CF::DeleteApp.new.execute(@app)
-  end
-
-  context 'deploying a basic PHP5 app that loads all prepackaged extensions' do
-    let(:app_name) { 'php_5_app_with_all_modules' }
-
+  shared_examples_for 'it loads all the modules' do |php_version|
     it 'logs each module on the info page' do
       expect(@app).to be_running
       browser.visit_path('/')
-      php_version_5['modules'].each do |module_name|
-        expect(browser).to have_body Regexp.new("module_#{module_name}", Regexp::IGNORECASE)
+      php_modules[php_version]['modules'].each do |module_name|
+        expect(browser).to have_body /module_(Zend[+ ])?#{module_name}/i
       end
     end
   end
 
-  context 'deploying a basic PHP7 app that loads all prepackaged extensions' do
-    let(:app_name) { 'php_7_app_with_all_modules' }
-
-    it 'logs each module on the info page' do
-      expect(@app).to be_running
-      browser.visit_path('/')
-      php_version_7['modules'].each do |module_name|
-        expect(browser).to have_body Regexp.new("module_#{module_name}", Regexp::IGNORECASE)
+  context 'extensions are specified in .bp-config' do
+    context 'deploying a basic PHP5 app that loads all prepackaged extensions' do
+      before(:all) do
+        @app = setup_app('php_5_app_with_all_modules')
       end
+
+      after(:all) do
+        Machete::CF::DeleteApp.new.execute(@app)
+      end
+
+      it_behaves_like 'it loads all the modules', 'PHP 5'
+    end
+
+    context 'deploying a basic PHP7 app that loads all prepackaged extensions' do
+      before(:all) do
+        @app = setup_app('php_7_app_with_all_modules')
+      end
+
+      after(:all) do
+        Machete::CF::DeleteApp.new.execute(@app)
+      end
+
+      it_behaves_like 'it loads all the modules', 'PHP 7'
+    end
+  end
+
+  context 'extensions are specified in composer.json' do
+    context 'deploying a basic PHP5 app that loads all prepackaged extensions' do
+      before(:all) do
+        @app = setup_app('php_5_app_with_all_modules_using_composer')
+      end
+
+      after(:all) do
+        Machete::CF::DeleteApp.new.execute(@app)
+      end
+
+      it_behaves_like 'it loads all the modules', 'PHP 5'
+    end
+
+    context 'deploying a basic PHP7 app that loads all prepackaged extensions' do
+      before(:all) do
+        @app = setup_app('php_7_app_with_all_modules_using_composer')
+      end
+
+      after(:all) do
+        Machete::CF::DeleteApp.new.execute(@app)
+      end
+
+      it_behaves_like 'it loads all the modules', 'PHP 7'
     end
   end
 end
