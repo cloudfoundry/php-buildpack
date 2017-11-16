7a452087908bcb49f9115435c659ba0196b1bae0
Jackson Feeny <jacksonfeeny@gmail.com>
2017-11-16 10:45:30 -0500

    Update cassandra integration test to allow setting a cassandra host [#147918041]
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- cf_spec/integration/deploy_a_php_app_with_cassandra_spec.rb
+++ cf_spec/integration/deploy_a_php_app_with_cassandra_spec.rb
@@ -10,8 +10,9 @@ describe 'CF PHP Buildpack' do
       {env: {'COMPOSER_GITHUB_OAUTH_TOKEN' => ENV['COMPOSER_GITHUB_OAUTH_TOKEN']}}
     )
 
+    @cassandra_host = ENV['CASSANDRA_HOST'] || "1.1.1.1"
     @service_name = "cassandra-test-service-php-#{Random.rand(100000)}"
-    Machete::SystemHelper.run_cmd(%(cf cups #{@service_name} -p '{"username":"uname","password":"pwd","node_ips":["1.1.1.1","2.2.2.2"]}'))
+    Machete::SystemHelper.run_cmd(%(cf cups #{@service_name} -p '{"username":"uname","password":"pwd","node_ips":["#{@cassandra_host}"]}'))
     Machete::SystemHelper.run_cmd(%(cf bind-service #{@app_name} #{@service_name}))
     Machete::SystemHelper.run_cmd(%(cf unbind-service #{@app_name} #{@app_name}-test-service))
     Machete::SystemHelper.run_cmd(%(cf restage #{@app_name}))
@@ -28,10 +29,17 @@ describe 'CF PHP Buildpack' do
         expect(@app).to be_running
       end
 
-      it 'logs that Cassandra could not connect to a server' do
-        expect { browser.visit_path('/') }.to raise_error(Machete::HTTPServerError)
-        expect(@app).to have_logged 'No hosts available for the control connection'
-        expect(@app).to have_logged 'Cassandra\\\\DefaultCluster->connect()'
+      if ENV['CASSANDRA_HOST']
+        it 'connects to and queries Cassandra server' do
+          browser.visit_path('/')
+          expect(browser).to have_body '<tr><td>system_auth</td><td>roles</td></tr>'
+        end
+      else
+        it 'logs that Cassandra could not connect to a server' do
+          expect { browser.visit_path('/') }.to raise_error(Machete::HTTPServerError)
+          expect(@app).to have_logged 'No hosts available for the control connection'
+          expect(@app).to have_logged 'Cassandra\\\\DefaultCluster->connect()'
+        end
       end
     end
   end
