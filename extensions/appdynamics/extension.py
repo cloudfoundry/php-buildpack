45664503e8b7d9bf0eebf44be77ebb2669644f83
Rkoppul <ram.koppula@gmail.com>
2018-05-01 15:17:07 -0700

    Reverting AppDynamics version to "4.3.5.9"

--- extensions/appdynamics/extension.py
+++ extensions/appdynamics/extension.py
@@ -51,8 +51,8 @@ class AppDynamicsInstaller(PHPExtensionHelper):
         """
         return {
                 'APPDYNAMICS_HOST': 'packages.appdynamics.com',
-                'APPDYNAMICS_VERSION': '4.4.2.849',
-                'APPDYNAMICS_PACKAGE': 'appdynamics-php-agent-linux_x64-{APPDYNAMICS_VERSION}.tar.bz2',
+                'APPDYNAMICS_VERSION': '4.3.5.9',
+                'APPDYNAMICS_PACKAGE': 'appdynamics-php-agent-x64-linux-{APPDYNAMICS_VERSION}.tar.bz2',
                 'APPDYNAMICS_DOWNLOAD_URL': 'https://{APPDYNAMICS_HOST}/php/{APPDYNAMICS_VERSION}/{APPDYNAMICS_PACKAGE}'
         }
 
