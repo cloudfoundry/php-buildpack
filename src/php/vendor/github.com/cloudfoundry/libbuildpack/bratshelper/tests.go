f6fba4e2bf86d1fd3efedf50a30fe4afb7f547dc
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2018-06-25 18:48:25 +0000

    Update libbuildpack

--- src/php/vendor/github.com/cloudfoundry/libbuildpack/bratshelper/tests.go
+++ src/php/vendor/github.com/cloudfoundry/libbuildpack/bratshelper/tests.go
@@ -67,6 +67,16 @@ func DeployingAnAppWithAnUpdatedVersionOfTheSameBuildpack(copyBrats func(string)
 		AfterEach(func() {
 			defaultCleanup(app)
 			Expect(cutlass.DeleteBuildpack(bpName)).To(Succeed())
+			// With stacks, creating the buildpack twice will result in a second record, one with `nil` stack.
+			// We need to clean up both.
+			if count, err := cutlass.CountBuildpack(bpName); err == nil && count > 0 {
+				Expect(cutlass.DeleteBuildpack(bpName)).To(Succeed())
+			}
+			// LTS errors when running `cf buildpacks`. Ignore that output.
+			count, err := cutlass.CountBuildpack(bpName)
+			if err == nil {
+				Expect(count).To(BeZero())
+			}
 		})
 
 		It("prints useful warning message to stdout", func() {
