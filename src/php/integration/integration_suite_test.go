701c35e4da50e1dbc8bab055e7693f4b81c657f9
Leah Hanson <lhanson@pivotal.io>
2018-07-09 11:55:04 -0400

    Improve integration test error message.
    You have to set an env variable when running integration tests locally.
    
    Signed-off-by: Jackson Feeny <jacksonfeeny@gmail.com>

--- src/php/integration/integration_suite_test.go
+++ src/php/integration/integration_suite_test.go
@@ -34,7 +34,7 @@ func init() {
 
 var _ = SynchronizedBeforeSuite(func() []byte {
 	// Run once
-	Expect(os.Getenv("COMPOSER_GITHUB_OAUTH_TOKEN")).ToNot(BeEmpty()) // Required for some tests
+	Expect(os.Getenv("COMPOSER_GITHUB_OAUTH_TOKEN")).ToNot(BeEmpty(), "Please set COMPOSER_GITHUB_OATH_TOKEN") // Required for some tests
 
 	if buildpackVersion == "" {
 		packagedBuildpack, err := cutlass.PackageUniquelyVersionedBuildpack(os.Getenv("CF_STACK"))
