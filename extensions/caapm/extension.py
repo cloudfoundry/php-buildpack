7ebaf0b28af2d608d7ed8ea9e9f2382f8c56234e
Tyler Phelan <tphelan@pivotal.io>
2019-01-28 10:23:18 -0500

    Update CAAPM fixture for new version
    [#163449889]
    
    Co-authored-by: Don Nguyen <dnguyen@pivotal.io>

--- extensions/caapm/extension.py
+++ extensions/caapm/extension.py
@@ -59,7 +59,7 @@ class CAAPMInstaller(object):
 
         return {
             'CA_APM_DOWNLOAD_HOST': 'ca.bintray.com/apm-agents',
-            'CA_APM_DOWNLOAD_VERSION': '10.7.0',
+            'CA_APM_DOWNLOAD_VERSION': '10.7.3',
             'CA_APM_PHP_PACKAGE': 'CA-APM-PHPAgent-{CA_APM_DOWNLOAD_VERSION}_linux.tar.gz',
             'CAAPM_DOWNLOAD_URL': 'https://{CA_APM_DOWNLOAD_HOST}/{CA_APM_PHP_PACKAGE}'
         }
