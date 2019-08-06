68dae28686b2251685f47c8749355d81d1876288
Daniel Mikusa <dmikusa@pivotal.io>
2019-08-06 10:51:56 -0400

    Add input arguments to password_hash function to protect from internal changes
    [#167682628]
    
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

--- fixtures/with_argon2/index.php
+++ fixtures/with_argon2/index.php
@@ -1,4 +1,8 @@
 hI<?php
-  print('password hash of "hello-world": ' . password_hash('hello-world!', PASSWORD_ARGON2I));
+    $options = [
+        'memory_cost' => 1024,
+        'time_cost' => 2
+    ];
+  print('password hash of "hello-world": ' . password_hash('hello-world!', PASSWORD_ARGON2I, $options));
 ?>
 
