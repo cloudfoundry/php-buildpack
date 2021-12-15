415c64f1eb7f2b56cdbf16a58d55d0dddfa9e465
Andrew Meyer <meyera@vmware.com>
2021-12-15 09:20:14 -0800

    Revert "Remove php 7.3 (not supported as of 12-06-2021)"
    This reverts commit eb5866cc420d6d353e037a2641c5a57118382e51.

--- src/php/integration/composer_with_invalid_json_test.go
+++ src/php/integration/composer_with_invalid_json_test.go
@@ -2,9 +2,10 @@ package integration_test
 
 import (
 	"os"
-	"os/exec"
+	"time"
 
 	"github.com/cloudfoundry/libbuildpack/cutlass"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -17,13 +18,8 @@ var _ = Describe("When composer.json is invalid JSON", func() {
 		app = cutlass.New(Fixtures("composer_invalid_json"))
 		app.SetEnv("COMPOSER_GITHUB_OAUTH_TOKEN", os.Getenv("COMPOSER_GITHUB_OAUTH_TOKEN"))
 		Expect(app.Push()).ToNot(Succeed())
+		Expect(app.ConfirmBuildpack(buildpackVersion)).To(Succeed())
 
-		logs := exec.Command("cf", "logs", "--recent", app.Name)
-		out, err := logs.CombinedOutput()
-		Expect(err).ToNot(HaveOccurred())
-
-		Expect(out).To(ContainSubstring("-------> Buildpack version " + buildpackVersion))
-
-		Expect(out).To(ContainSubstring("Invalid JSON present in composer.json. Parser said"))
+		Eventually(app.Stdout.String, 10*time.Second).Should(ContainSubstring("Invalid JSON present in composer.json. Parser said"))
 	})
 })
