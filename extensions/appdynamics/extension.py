e5fc76999d9a0557ba4d80991d3abcbc0ec1081c
Brayan Henao <bhenao@vmware.com>
2022-01-03 15:42:45 -0500

    Update remaining appdynamics references to an older version

--- extensions/appdynamics/extension.py
+++ extensions/appdynamics/extension.py
@@ -51,7 +51,7 @@ class AppDynamicsInstaller(PHPExtensionHelper):
         """
         return {
                 'APPDYNAMICS_HOST': 'packages.appdynamics.com',
-                'APPDYNAMICS_VERSION': '21.12.1.404',
+                'APPDYNAMICS_VERSION': '21.12.2.13',
                 'APPDYNAMICS_PACKAGE': 'appdynamics-php-agent-x64-linux-{APPDYNAMICS_VERSION}.tar.bz2',
                 'APPDYNAMICS_DOWNLOAD_URL': 'https://buildpacks.cloudfoundry.org/dependencies/manual-binaries/appdynamics/appdynamics-php-agent-x64-linux-{APPDYNAMICS_VERSION}.tar.bz2'
         }
