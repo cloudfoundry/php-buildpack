95e1385a5da799f0230723300b97b7b70c3a26be
Florian Arthofer <florian.arthofer@dynatrace.com>
2022-05-13 22:56:45 +0200

    added update_agent_config() to Dynatrace extension  (#644)
    * added update_agent_config() to Dynatrace extension to fetch most recent OneAgent config from configured tenant
    
    * added ruxitagentproc.conf file to fake dynatrace api

--- extensions/dynatrace/extension.py
+++ extensions/dynatrace/extension.py
@@ -23,6 +23,7 @@ from subprocess import call
 import urllib2
 import json
 import time
+import re
 
 _log = logging.getLogger('dynatrace')
 
@@ -106,7 +107,8 @@ class DynatraceInstaller(object):
                 f.write(result.read())
                 f.close()
                 return
-            except IOError, exc:
+            # TODO change to 'err' if this is correct
+            except IOError as exc:
                 last_exception = exc
                 waittime = base_waittime + 2**attempt
                 _log.warn("Error during installer download, retrying in %s seconds" % (waittime))
@@ -127,7 +129,7 @@ class DynatraceInstaller(object):
         try:
             self._retry_download(url, installer)
             os.chmod(installer, 0o777)
-        except IOError, exc:
+        except IOError as exc:
             if skiperrors == 'true':
                 _log.warn('Error during installer download, skipping installation: %s' % (exc))
                 self._run_installer = False
@@ -188,6 +190,136 @@ class DynatraceInstaller(object):
         with open(envfile, "a") as file:
             file.write(extra_env)
 
+    # downloading the most recent OneAgent config from the configured tenants API,
+    # and merging it with the static config the standalone installer package brought along
+    def update_agent_config(self):
+
+        skiperrors = self._ctx['DYNATRACE_SKIPERRORS']
+        agent_config_url = self._ctx['DYNATRACE_API_URL'] + '/v1/deployment/installer/agent/processmoduleconfig'
+
+        try:
+            # fetch most recent OneAgent config from tenant API
+            request = urllib2.Request(agent_config_url)
+            request.add_header("user-agent", "cf-php-buildpack/" + self.get_buildpack_version())
+            request.add_header("Authorization", "Api-Token {token}".format(token=self._ctx['DYNATRACE_TOKEN']))
+            result = urllib2.urlopen(request)
+        except IOError as err:
+            if skiperrors == 'true':
+                _log.warn('Error during agent config update, skipping it: %s'% (err))
+                return
+            else:
+                _log.error('ERROR: Failed to download most recent OneAgent config from API: %s '% (err))
+                raise
+        _log.debug("Successfully fetched OneAgent config from API")
+
+        # store fetched config in a nested dictionary for easy merging with
+        # the data from ruxitagentproc.conf
+        json_data = json.load(result)
+        config_from_api = dict()
+        for elem in json_data['properties']:
+            # Storing these values in individual variables might be a bit
+            # redundant, but it improves readability below.
+            # Also explicitly adding the braces for the sections we get via the
+            # the API, to have them formatted in the same ways as the ones from
+            # the ruxitagentproc.conf file
+            section = "[" + elem['section'] + "]"
+            key = elem['key']
+            value = elem['value']
+
+            # checking if the required dict is already there.
+            # if not: initialize it
+            if section not in config_from_api:
+                config_from_api[section] = dict()
+
+            config_from_api[section][key] = value
+
+
+        # read static config from standalone installer
+        try:
+            agent_config_path = os.path.join(
+                self._ctx['BUILD_DIR'],
+                'dynatrace',
+                'oneagent',
+                'agent',
+                'conf',
+                'ruxitagentproc.conf')
+            agent_config_file = open(agent_config_path, 'r')
+            agent_config_data = agent_config_file.readlines()
+            agent_config_file.close()
+        except IOERROR as err:
+            _log.error("ERROR: Failed to read OneAgent config file: %s" % (err))
+            raise
+
+        _log.debug("Successfully read OneAgent config from " + agent_config_path)
+
+        # store static config in same kind of data structure (nested dictionary)
+        # as we use for the config from we fetched from the API
+        section_regex = re.compile('\[(.*)\]')
+        config_section = ""
+        config_from_agent = dict()
+        _log.debug("Starting to parse OneAgent config...")
+        for line in agent_config_data:
+            line = line.rstrip()
+
+            if section_regex.match(line):
+                config_section = line
+                continue
+
+            if  line.startswith('#'):
+                # skipping over lines that are purely comments
+                continue
+            elif line == "":
+                # skipping over empty lines
+                continue
+
+            # store data in dict
+
+            # checking if the required dict is already there.
+            # if not: initialize it
+            if config_section not in config_from_agent:
+                config_from_agent[config_section] = dict()
+
+            config_line_key = line.split()[0]
+            rest_of_the_line = line.split()[1:]
+            # the join-construct is needed to convert the list, we get back from the slicing,
+            # into a proper string again.
+            #Otherwise we'd need to do the conversion whe writing the data back to the
+            # config file, which would be more cumbersome.
+            config_line_value = ' '.join(rest_of_the_line)
+            config_from_agent[config_section][config_line_key] = config_line_value
+        _log.debug("Successfully parsed OneAgent config...")
+
+        # Merging the two configs by just writing the contents we got from
+        # the API over the data we got from the local config file.
+        # This replaces existing values and adds new ones.
+        _log.debug("Starting with OneAgent configuration merging")
+        for section_name, section_content in config_from_api.items():
+            for key in section_content:
+                # checking if the required dict is already there.
+                # if not: initialize it
+                if section_name not in config_from_agent:
+                    config_from_agent[section_name] = dict()
+                config_from_agent[section_name][key] = config_from_api[section_name][key]
+        _log.debug("Finished OneAgent configuration merging")
+
+        # Write updated config back to ruxitagentproc.conf file
+        try:
+            overwrite_agent_config_file = open(agent_config_path, 'w')
+            for section_name, section_content in config_from_agent.items():
+                overwrite_agent_config_file.write(section_name + "\n")
+                for key in section_content:
+                    write_line = key + " " + section_content[key] + "\n"
+                    overwrite_agent_config_file.write(write_line)
+                # Trailing empty newline at the end of each section for better human readability
+                overwrite_agent_config_file.write("\n")
+            overwrite_agent_config_file.close()
+        except IOError as err:
+            _log.error("ERROR: Failed to write updated config to OneAgent config file: %s" % (err))
+            raise
+
+        _log.debug("Finished writing updated OneAgent config back to " + agent_config_path)
+
+
 
 # Extension Methods
 def compile(install):
@@ -204,4 +336,6 @@ def compile(install):
             dynatrace.adding_environment_variables()
             _log.info("Adding Dynatrace LD_PRELOAD settings")
             dynatrace.adding_ld_preload_settings()
+            _log.info("Fetching updated OneAgent configuration from tenant...")
+            dynatrace.update_agent_config()
     return 0
