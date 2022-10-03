707b48f0b15e2ed5aac80fdf15f8439016603fae
Brayan Henao <bhenao@vmware.com>
2022-10-03 10:32:32 -0400

    Update PHP default version
    Co-authored-by: Arjun Sreedharan <asreedharan@vmware.com>

--- tests/test_extension_helper.py
+++ tests/test_extension_helper.py
@@ -26,7 +26,7 @@ class TestPHPExtensionHelper(object):
     def test_basic(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29'
+            'PHP_VERSION': '7.4.32'
         })
         ext = PHPExtensionHelper(ctx)
         ext.load_config()
@@ -48,7 +48,7 @@ class TestPHPExtensionHelper(object):
     def test_merge_defaults(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29',
+            'PHP_VERSION': '7.4.32',
             'SOME_JUNK': 'jkl;'
         })
 
@@ -62,12 +62,12 @@ class TestPHPExtensionHelper(object):
         eq_(4, len(ext._ctx))
         eq_('asdf', ext._ctx['DEFAULT_JUNK'])
         eq_('jkl;', ext._ctx['SOME_JUNK'])
-        eq_('7.4.29', ext._ctx['PHP_VERSION'])
+        eq_('7.4.32', ext._ctx['PHP_VERSION'])
 
     def test_compile_runs(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29'
+            'PHP_VERSION': '7.4.32'
         })
 
         class MyExtn(PHPExtensionHelper):
@@ -82,7 +82,7 @@ class TestPHPExtensionHelper(object):
     def test_compile_doesnt_run(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29'
+            'PHP_VERSION': '7.4.32'
         })
 
         class MyExtn(PHPExtensionHelper):
@@ -97,7 +97,7 @@ class TestPHPExtensionHelper(object):
     def test_configure_runs(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29'
+            'PHP_VERSION': '7.4.32'
         })
 
         class MyExtn(PHPExtensionHelper):
@@ -112,7 +112,7 @@ class TestPHPExtensionHelper(object):
     def test_configure_doesnt_run(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29'
+            'PHP_VERSION': '7.4.32'
         })
 
         class MyExtn(PHPExtensionHelper):
@@ -127,7 +127,7 @@ class TestPHPExtensionHelper(object):
     def test_preprocess_commands_runs(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29'
+            'PHP_VERSION': '7.4.32'
         })
 
         class MyExtn(PHPExtensionHelper):
@@ -142,7 +142,7 @@ class TestPHPExtensionHelper(object):
     def test_preprocess_commands_doesnt_run(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29'
+            'PHP_VERSION': '7.4.32'
         })
 
         class MyExtn(PHPExtensionHelper):
@@ -157,7 +157,7 @@ class TestPHPExtensionHelper(object):
     def test_service_commands_runs(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29'
+            'PHP_VERSION': '7.4.32'
         })
 
         class MyExtn(PHPExtensionHelper):
@@ -172,7 +172,7 @@ class TestPHPExtensionHelper(object):
     def test_service_commands_doesnt_run(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29'
+            'PHP_VERSION': '7.4.32'
         })
 
         class MyExtn(PHPExtensionHelper):
@@ -187,7 +187,7 @@ class TestPHPExtensionHelper(object):
     def test_service_environment_runs(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29'
+            'PHP_VERSION': '7.4.32'
         })
 
         class MyExtn(PHPExtensionHelper):
@@ -202,7 +202,7 @@ class TestPHPExtensionHelper(object):
     def test_service_environment_doesnt_run(self):
         ctx = utils.FormattedDict({
             'BUILD_DIR': self.build_dir,
-            'PHP_VERSION': '7.4.29'
+            'PHP_VERSION': '7.4.32'
         })
 
         class MyExtn(PHPExtensionHelper):
