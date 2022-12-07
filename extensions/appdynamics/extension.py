dec49118b3c111a1df3cbb914fd2acc31b1dd766
Cloud Foundry Buildpacks Team Robot <cf-buildpacks-eng@pivotal.io>
2022-12-07 10:41:34 -0500

    Add appdynamics 22.12.0-667, remove appdynamics 22.11.0-656 (#738)
    for stack(s) cflinuxfs3 [#183979217]

--- extensions/appdynamics/extension.py
+++ extensions/appdynamics/extension.py
@@ -51,7 +51,7 @@ class AppDynamicsInstaller(PHPExtensionHelper):
         """
         return {
                 'APPDYNAMICS_HOST': 'download.run.pivotal.io',
-                'APPDYNAMICS_VERSION': '22.11.0-656',
+                'APPDYNAMICS_VERSION': '22.12.0-667',
                 'APPDYNAMICS_PACKAGE': 'appdynamics-{APPDYNAMICS_VERSION}.tar.bz2',
                 'APPDYNAMICS_DOWNLOAD_URL': 'https://{APPDYNAMICS_HOST}/appdynamics-php/{APPDYNAMICS_PACKAGE}'
         }
