9461bfb51a0413f0d20414bc17a2c79a7aff1bd9
Brayan Henao <bhenao@vmware.com>
2023-02-28 15:57:30 -0500

    Fix scripts/unit.sh errors (#829)
    Co-authored-by: Sophie Wigmore <swigmore@vmware.com>

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+readonly ROOTDIR
+
 source .envrc
+
+# shellcheck source=SCRIPTDIR/.util/tools.sh
+source "${ROOTDIR}/scripts/.util/tools.sh"
+
+util::tools::ginkgo::install --directory "${ROOTDIR}/.bin"
 
 cd src/*/integration/..
 export CF_STACK=${CF_STACK:-cflinuxfs3}
