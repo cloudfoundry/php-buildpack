1baa4f633b2336be4b4527ded1a8271f5da21259
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2023-04-28 09:48:04 +0000

    Add appdynamics 23.4.0-724, remove appdynamics 23.2.0-684
    for stack(s) cflinuxfs4, cflinuxfs3 [#185059642]

--- extensions/appdynamics/extension.py
+++ extensions/appdynamics/extension.py
@@ -51,7 +51,7 @@ class AppDynamicsInstaller(PHPExtensionHelper):
         """
         return {
                 'APPDYNAMICS_HOST': 'download.run.pivotal.io',
-                'APPDYNAMICS_VERSION': '23.2.0-684',
+                'APPDYNAMICS_VERSION': '23.4.0-724',
                 'APPDYNAMICS_PACKAGE': 'appdynamics-{APPDYNAMICS_VERSION}.tar.bz2',
                 'APPDYNAMICS_DOWNLOAD_URL': 'https://{APPDYNAMICS_HOST}/appdynamics-php/{APPDYNAMICS_PACKAGE}'
         }
